5ba0044b1d4a7b8081c48ebab81f0ce4e9db67c3
ubuntu_machine <eleusispc@live.com.mx>
2022-08-28 12:21:49 -0700

    Modifying algorithm from list_total_sum.c

--- lis_total_sum.c
+++ lis_total_sum.c
@@ -1,35 +1,32 @@
 #include <stdio.h>
 #define SUM 15
 typedef struct find_num_res{
-    int n1;
-    int n2;
+    int n;
+    int f;
 }pairs;
 
-void Find_SUM (int Array[],int l_ong , pairs *r);
+void Find_SUM (int l_ong , pairs r[]);
 int main(){
-    pairs r;
-    r.n1 = 0;
-    r.n2 = 0;
-    int my_array[] = {6,8,2,9,6,5,34,8,12,13,14,20};
-    int l =0; 
-    l = sizeof(my_array)/sizeof(my_array[0]);
-    Find_SUM(my_array,l,&r);
-    printf("Las posiciones que dan la suma de %d son %d y %d : ",SUM, r.n1,r.n2);
+    pairs r[]={{1,0},{5,0},{15,0},{9,0},{4,0},{7,0},{10,0},{5,0}};
+    int l = sizeof(r)/sizeof(r[0]);
+    Find_SUM(l,r);
+    printf("Las posiciones que dan de resultado %d\n",SUM);
+    for(int i = 0; i< l; i++){
+        if(r[i].f == 1){
+            printf("%d,",i);
+        }
+    }
+    
     return 0;
 }
-void Find_SUM (int Array[],int l_ong,pairs *r){
-    
-    int f = 0;
-    for(int i = 0; i < l_ong; i ++){
-        for(int j = i + 1; j < (l_ong - (i+1)); j++){
-            if((Array[i] + Array[j]) == SUM){
-                r->n1 = i;
-                r->n2 = j;
-                f = 1;
-            }
-            if (f == 1){
-                break;
+void Find_SUM (int l_ong,pairs r[]){
+    for(int i = 0; i < l_ong; i++){
+        for(int j = i; j<l_ong-1;j++){
+            if((r[i].n + r[j].n) == SUM){
+                r[i].f = 1;
+                r[j].f = 1;
             }
         }
     }
+    
 }
